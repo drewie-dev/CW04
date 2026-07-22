46bdb014cf70715325e2af6d8203f5cccfaff704
Alexander Khauv <4changt@gmail.com>
2026-07-22 13:22:19 -0400

    Add filtered produce list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,72 @@
 import React, { Component } from "react";
+import { DropdownButton, MenuItem } from "react-bootstrap";
+import List from "./List";
 
-class List extends Component {
+class FilteredList extends Component {
+  constructor(props) {
+    super(props);
+
+    // Stores the search text and selected filter
+    this.state = {
+      search: "",
+      type: "All"
+    };
+  }
+
+  // Updates the search box
+  onSearch = (event) => {
+    this.setState({
+      search: event.target.value.toLowerCase()
+    });
+  };
+
+  // Updates the dropdown filter
+  onFilter = (eventKey) => {
+    this.setState({
+      type: eventKey
+    });
+  };
+
+  // Checks if an item matches the search and filter
+  filterItem = (item) => {
+    const matchesSearch =
+      item.name.toLowerCase().search(this.state.search) !== -1;
+
+    const matchesType =
+      this.state.type === "All" ||
+      item.type === this.state.type;
+
+    return matchesSearch && matchesType;
+  };
+
   render() {
+    // Creates a filtered list to display
+    const filteredItems = this.props.items.filter(this.filterItem);
+
     return (
-      <ul className="produce-list">
-        {this.props.items.map((item) => (
-          <li key={item.name}>
-            {item.name} - {item.type}
-          </li>
-        ))}
-      </ul>
+      <div>
+        {/* Search box */}
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={this.onSearch}
+        />
+
+        {/* Dropdown menu */}
+        <DropdownButton
+          title="Filter"
+          onSelect={this.onFilter}
+        >
+          <MenuItem eventKey="All">All</MenuItem>
+          <MenuItem eventKey="Fruit">Fruit</MenuItem>
+          <MenuItem eventKey="Vegetable">Vegetables</MenuItem>
+        </DropdownButton>
+
+        {/* Display the filtered items */}
+        <List items={filteredItems} />
+      </div>
     );
   }
 }
 
-export default List;
+export default FilteredList;
